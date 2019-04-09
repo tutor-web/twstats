99478f20eb2858906dad4e0df42f0638a54ecc07
Jamie Lentin <jm@lentin.co.uk>
2019-04-09 14:45:24 +0100

    eurostat: Add country comparison tables

--- R/source_eurostat.R
+++ R/source_eurostat.R
@@ -22,6 +22,37 @@ register_all_countries <- function (tbl, sub_rowcount) {
     }
 }
 
+register_country_comparisons <- function (tbl, sub_rowcount) {
+    # Country codes we are interested in
+    twstat_countries <- structure(eurostat::eu_countries$name, names = eurostat::eu_countries$code)
+
+    filter_data_fn <- function(sel_countries) {
+        force(sel_countries)  # NB: http://www.win-vector.com/blog/2017/02/iteration-and-closures-in-r/
+        function () {
+            # Get values for each country, rename value columns
+            tbl_1 <- tbl$data()[country == names(sel_countries)[[1]],]
+            data.table::setnames(tbl_1, gsub('^value$', names(sel_countries)[[1]], strsplit(tbl$columns, '/')[[1]]))
+            tbl_1[,country := NULL]
+
+            tbl_2 <- tbl$data()[country == names(sel_countries)[[2]],]
+            data.table::setnames(tbl_2, gsub('^value$', names(sel_countries)[[2]], strsplit(tbl$columns, '/')[[1]]))
+            tbl_2[,country := NULL]
+
+            return(merge(tbl_1, tbl_2))
+        }
+    }
+
+    # For all pairs of countries...
+    for (sel_countries in utils::combn(twstat_countries, 2, simplify = FALSE)) {
+        s <- twstats_table(paste0(tbl$name, '/', paste0(names(sel_countries), collapse = "-")),
+            columns = gsub('(^|/)country(/|$)', '\\1value\\2', tbl$columns),
+            rowcount = sub_rowcount,
+            title = paste0(tbl$title, ' in ', paste0(sel_countries, collapse = " vs ")),
+            data = filter_data_fn(sel_countries))
+        twstats_register_table(s)
+    }
+}
+
 twstats_register_eurostat <- function () {
     if (!requireNamespace('eurostat', quietly = TRUE)) {
         warning("eurostat package not available, not registering eurostat tables")
@@ -43,4 +74,5 @@ twstats_register_eurostat <- function () {
         })
     twstats_register_table(tin00073)
     register_all_countries(tin00073, 10)
+    register_country_comparisons(tin00073, 10)
 }
